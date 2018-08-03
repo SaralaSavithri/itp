ab7e8a0bfb6c963403f9cfbd6547f59200f91dc7
Sarala <Sarala@DESKTOP-ON5Q0V8>
2018-08-03 21:55:21 +0530

    sarala

--- ITP/src/Sarala.java
+++ ITP/src/Sarala.java
@@ -1,4 +1,7 @@
 
 public class Sarala {
-
+	public static void main(String arg[]) {
+	
+	System.out.println("Sarala");
+	}
 }
--- ITP/src/Test.java
+++ ITP/src/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		System.out.println("Hello world");
 		System.out.println("New Change");
 		System.out.println("Gon Deshan");
+	
 	}
 	
 }
